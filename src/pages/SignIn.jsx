26c8dc49a3dac03facbc7c514eb87bbfbcc38fa5
snowone <snowone4426@gmail.com>
2022-09-20 16:06:24 +0900

    [Create] 로그인 페이지 기본 레이아웃 및 기능 구현
     - 서버의 부제로 고정값으로 정보 저장
     - 로그인 기능 구현
     - 로그인시 메인페이지로 이동
     - 메인페이지에서 뒤로가기 시 로그인 페이지로 오지 못하도록 함.
     - 주소를 통해 로그인페이지에 접근시 메인페이지로 강제이동
     - 정보 모두 입력시 로그인 버튼 점등

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,127 @@
+import { useEffect, useState } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 
-const SignIn = () => {
-  return <div>SignIn</div>
+const SignIn = ({ signinHanlder }) => {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const [inputInfo, setInputInfo] = useState({ id: '', password: '' })
+
+  useEffect(() => {
+    if (!location.state || !['nav', 'signup'].includes(location.state.where)) {
+      navigate('/')
+    }
+  }, [])
+
+  const inputHanlder = (e) => {
+    setInputInfo((inputInfo) => {
+      return { ...inputInfo, [e.target.id]: e.target.value }
+    })
+  }
+
+  const submitHanlder = () => {
+    if (inputInfo.id === 'test' && inputInfo.password === 'test') {
+      signinHanlder()
+      navigate('/', { replace: true })
+    } else {
+      setInputInfo({ id: '', password: '' })
+      alert('틀렷슈')
+    }
+  }
+
+  return (
+    <SignInContainer>
+      <InputContainer>
+        <LoginTitle>Logo</LoginTitle>
+        <InputLabel htmlFor="id">
+          <LoginInput
+            type="text"
+            id="id"
+            value={inputInfo.id}
+            placeholder="ID"
+            onChange={inputHanlder}
+          />
+        </InputLabel>
+        <InputLabel htmlFor="password">
+          <LoginInput
+            type="password"
+            id="password"
+            value={inputInfo.password}
+            placeholder="PASSWORD"
+            onChange={inputHanlder}
+          />
+        </InputLabel>
+        <LinkContainer>
+          <MiniBtn onClick={() => navigate('/signup')}>회원가입</MiniBtn>
+          <MiniBtn>비밀번호 찾기</MiniBtn>
+        </LinkContainer>
+        <SubmitBtn inputInfo={inputInfo} onClick={submitHanlder}>
+          로그인
+        </SubmitBtn>
+      </InputContainer>
+    </SignInContainer>
+  )
 }
+
+const SignInContainer = styled.main`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  min-height: 80vh;
+`
+
+const InputContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 470px;
+`
+
+const LoginTitle = styled.header`
+  font-size: 40px;
+  font-weight: 600;
+  margin-bottom: 40px;
+`
+
+const InputLabel = styled.label`
+  display: flex;
+  align-items: center;
+  width: 450px;
+  height: 70px;
+  border: 2px solid #bb905b;
+  border-bottom: ${({ htmlFor }) =>
+    htmlFor === 'id' ? '0px' : '2px solid #bb905b'};
+  padding: 10px;
+`
+
+const LoginInput = styled.input`
+  font-size: 32px;
+`
+
+const LinkContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 450px;
+  padding: 5px 2px;
+`
+
+const MiniBtn = styled.div`
+  font-size: 15px;
+  font-weight: 600px;
+  color: #665e5c;
+  margin-left: 4px;
+  cursor: pointer;
+`
+
+const SubmitBtn = styled.button`
+  width: 450px;
+  height: 54px;
+  font-size: 40px;
+  color: #f1f1f1;
+  border-radius: 5px;
+  background-color: ${({ inputInfo }) =>
+    inputInfo.id.length && inputInfo.password.length ? '#9F6255' : '#958380'};
+`
 
 export default SignIn
